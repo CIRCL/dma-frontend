8655cd13f51642a195c59247fa20938a73873d44
Steve Clement <steve@localhost.lu>
2016-02-17 11:51:17 +0100

    - Added multi instance functionality on all levels. One more binary presence test

--- bin/submit.sh
+++ bin/submit.sh
@@ -1,14 +1,13 @@
 #!/bin/bash
 
-CUCKOO_API_URL=("http://localhost:8090")
-#CUCKOO_API_URL=("http://localhost:8090" "http://crg:8090")
+CUCKOO_API_URL=("http://localhost:8090" "http://crg:8090")
 CUCKOO_API_TASKS_CREATE_FILE="/tasks/create/file"
 CUCKOO_API_TASKS_VIEW="/tasks/view/"
 CUCKOO_STATUS="/cuckoo/status"
-CUCKOO_VERSION=`curl -s ${CUCKOO_API_URL[0]}${CUCKOO_STATUS} |jq -r .version`
 CUCKOO_COUNT=`echo ${#CUCKOO_API_URL[@]}`
-echo -n "Checking for redis-cli (wants to be in \$PATH) will do exit 1 if not found. "
+echo -n "Checking for redis-cli and mutt (want to be in \$PATH) will exit 1 if not found. "
 REDISCLI=`which redis-cli` && echo "Got redis-cli continuing…" || exit 1
+MUTTCMD=`which mutt` && echo "Got mutt continuing…" || exit 1
 
 GPG_ENABLE=false
 
@@ -27,13 +26,38 @@ function submitAdmin()
     done
 }
 
-# Cheap check to see if the cuckoo API is running
-if [ "${CUCKOO_VERSION}" != "" ]; then
-    echo "You run cuckoo version ${CUCKOO_VERSION}"
-else
-    echo "Could not get cuckoo version from API, perhaps api.py not running?"
-    exit -1
-fi
+function submitMail()
+{
+    s=`curl ${CUCKOO_API_URL[$1]}${CUCKOO_API_TASKS_VIEW}${task_id} >/tmp/c-$$`
+    if [ "$GPG_ENABLE" = true ]; then
+        fe=`gpg -e -o /tmp/e-$$.gpg -r ${SUBMISSION_MAIL} ${file}`
+        smail=`$MUTTCMD -a /tmp/e-$$.gpg -s "New DMA analysis submitted ${task_id} by ${user}" -- ${SUBMISSION_MAIL} </tmp/c-$$`
+        rm /tmp/e-$$.gpg
+    else
+        smail=`$MUTTCMD -s "New DMA analysis submitted ${task_id} by ${user}" -- ${SUBMISSION_MAIL} < /tmp/c-$$`
+    fi
+}
+
+
+function checkAPI()
+{
+    echo "# of cuckoo instances : ${CUCKOO_COUNT}"
+    for (( i=0; i<${CUCKOO_COUNT}; i++ ));
+    do
+        echo "Checking API ${CUCKOO_API_URL[i]}${CUCKOO_STATUS}"
+        CUCKOO_VERSION=`curl -s ${CUCKOO_API_URL[i]}${CUCKOO_STATUS} |jq -r .version`
+        # Cheap check to see if the cuckoo API is running
+        if [ "${CUCKOO_VERSION}" != "" ]; then
+            echo "You run cuckoo version ${CUCKOO_VERSION} on cuckoo instance ${i}"
+        else
+            echo "Could not get cuckoo version from API, perhaps api.py not running?"
+            echo "Check API on ${CUCKOO_API_URL[i]}${CUCKOO_STATUS}"
+            exit -1
+        fi
+    done
+}
+
+checkAPI
 
 while true
 do
@@ -60,23 +84,17 @@ do
         if [ "$CUCKOO_VERSION" == "2.0-dev" ]; then
             task_id=`curl -F package=${package} -F machine=${machine} -F file=@${file} ${CUCKOO_API_URL[i]}${CUCKOO_API_TASKS_CREATE_FILE} | jq -r .task_id | grep '[0-9]' |xargs`
             status=$($REDISCLI -n 5 SADD t:${user}:HEAD ${task_id})
+            echo "task_id ${task_id}"
             submitAdmin
+            submitMail $i
         elif [ "$CUCKOO_VERSION" = "1.3-Optiv" ] || [ "$CUCKOO_VERSION" = "1.3-NG" ]; then
             task_id=`curl -F package=${package} -F machine=${machine} -F file=@${file} ${CUCKOO_API_URL[i]}${CUCKOO_API_TASKS_CREATE_FILE} | jq -r .task_ids | grep '[0-9]' |xargs`
+            echo "task_id ${task_id}"
             status=$($REDISCLI -n 5 SADD t:${user}:modified ${task_id})
             submitAdmin
+            submitMail $i
         fi
         done
-        echo "task_id ${task_id}"
-        status=$($REDISCLI -n 5 SADD t:${user} ${task_id})
-        s=`curl ${CUCKOO_API_URL[0]}${CUCKOO_API_TASKS_VIEW}${task_id} >/tmp/c-$$`
-        if [ "$GPG_ENABLE" = true ]; then
-            fe=`gpg -e -o /tmp/e-$$.gpg -r ${SUBMISSION_MAIL} ${file}`
-            smail=`mutt -a /tmp/e-$$.gpg -s "New DMA analysis submitted ${task_id} by ${user}" -- ${SUBMISSION_MAIL} </tmp/c-$$`
-            rm /tmp/e-$$.gpg
-        else
-            smail=`mutt -s "New DMA analysis submitted ${task_id} by ${user}" -- ${SUBMISSION_MAIL} < /tmp/c-$$`
-        fi
     done
     sleep 10
 done
