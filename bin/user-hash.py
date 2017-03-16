2ea5cf228eb49d030462bc7ad2fd26b1a09be50c
Steve Clement <steve@localhost.lu>
2017-03-16 14:59:19 +0100

    - user-hash.py now adds or updates user in DMAusers.json file

--- bin/user-hash.py
+++ bin/user-hash.py
@@ -1,18 +1,49 @@
 #!/usr/bin/env python3
 
 import sys
+import json
+import string
+import random
+import argparse
 
 try:
     import bcrypt
 except ImportError:
     sys.exit("Install bcrypt please")
 
+parser = argparse.ArgumentParser()
+
+parser = argparse.ArgumentParser(description='Add User to DMA')
+parser.add_argument("-u", "--user", required=True, help="Adds User to DMAusers.py and prints out a password.")
+
+args = parser.parse_args()
+
+def randomPassword():
+    chars = string.ascii_uppercase + string.ascii_lowercase + string.digits
+    size = random.randint(16, 32)
+    return ''.join(random.choice(chars) for x in range(size))
+
+plainText = randomPassword()
+
 users = {
-    "yourBasicAuthAdminUsername": "myPlainTextPassword",
+    args.user:plainText,
 }
 
-print("# Either create or add the above to: web/DMAusers.py")
+try:
+    with open('../web/DMAusers.json') as json_data_file:
+        usersFromFile = json.load(json_data_file)
+except OSError:
+        usersFromFile = {}
+
+print("# User will be added or updated in web/DMAusers.json")
 print("users = {")
 for key in users:
-    print("    '{}': {},".format(key, bcrypt.hashpw(users[key].encode('utf-8'), bcrypt.gensalt(16))))
+    hashSalt = bcrypt.hashpw(users[key].encode('utf-8'), bcrypt.gensalt(16))
+    print("    '{}': '{}', # <---- Plain-text version of password".format(key, plainText))
+    print("    '{}': {},".format(key, hashSalt))
+    users = { key:hashSalt.decode() }
+    usersFromFile.update(users)
 print("}")
+
+with open('../web/DMAusers.json', 'w') as outfile:
+    json.dump(usersFromFile, outfile)
